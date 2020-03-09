e13c31086706042809f107ed8f0338dd8a70deb4
LiemAlvin14 <57980019+LiemAlvin14@users.noreply.github.com>
2020-03-09 19:49:42 +0700

    Update Main.java

--- FindTheVolume/src/Main.java
+++ FindTheVolume/src/Main.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
 public class Main {
-    //changed from static to not static
-    int answer;
+    //remove answer fielld to avoid temporary field problem
     
     //refactor
-    private void menu(){
+    private int menu(){
         System.out.println();
         System.out.println("Type 1 to find the properties of a rectangle");
         System.out.println("Type 2 to find the properties of a sphere");
         System.out.println("Type something else to exit");
-        answer = input.nextInt();
+        int answer = input.nextInt();
+        return answer;
     }
     
     //refactor
-    private void (Shape)
-    
-    public Main(){
-        Scanner input = new Scanner (System.in);
-        while (true){
-            menu();
-            
-            if (answer == 1){
-            RectangularPrism r1 = new RectangularPrism();
-            r1.getValues();
-            //r1.calculateValues();
-            r1.displayValues();
-            
-            } else if (answer == 2){
-            System.out.println("What is the radius of the sphere");
-            Sphere s1 = new Sphere(input.nextDouble());
+    private void showMenu(int answer){
+        if(answer==1){
+            RectangularPrism s1 = new RectangularPrism();
+            s1.getValues();
             s1.calculateValues();
             s1.displayValues();
-            
+        }else{
+            Sphere() s1 = new Sphere();
+            s1.getValues();
+            s1.calculateValues();
+            s1.displayValues();
+        }
+    }
+    public Main(){
+        int answer=0;
+        Scanner input = new Scanner (System.in);
+        while (true){
+            answer=menu();
+            if (answer == 1 || answer == 2){
+               showMenu(answer);
             } else {
-                   break; 
-                }
+               break; 
             }
         }
     }
