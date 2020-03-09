191a29ee565e0dce40fe90e0009b0bafd99b765c
LiemAlvin14 <57980019+LiemAlvin14@users.noreply.github.com>
2020-03-09 19:27:03 +0700

    Create Shape3d.java

---
+++ FindTheVolume/src/Shape3d.java
@@ -0,0 +1,7 @@
+//this is a new class I made to refactor the code
+abstract class Shape3d{
+    
+    public void getValues();
+    public void calculateValues();
+    public void displayValues();
+}
