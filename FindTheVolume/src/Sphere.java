2f4f0110abd57d94c191a96d7ee4bf3fafaf3856
LiemAlvin14 <57980019+LiemAlvin14@users.noreply.github.com>
2020-03-09 19:45:25 +0700

    Update Sphere.java

--- FindTheVolume/src/Sphere.java
+++ FindTheVolume/src/Sphere.java
@@ -1,4 +1,6 @@
-public class Sphere {
+import java.util.Scanner;
+
+public class Sphere extends Shape3d{
     double radius;
     double surfaceArea;
     double volume;
@@ -7,11 +9,20 @@ public class Sphere {
         this.radius = rad;
     }
     
+    //refactor
+    @override
+    public void getValues(){
+        System.out.println("What is the radius of the sphere");
+        this.radius = input.nextInt();
+    }
+    
+    @override
     public void calculateValues(){
         this.surfaceArea = 4*3.14*this.radius*this.radius;
         this.volume = (this.radius*this.radius*this.radius)*(4/3)*3.14;
     }
     
+    @override
     public void displayValues(){
         System.out.println("The volume of the sphere is " + this.volume);
         System.out.println("The surface area of the sphere is " + this.surfaceArea);
